e5a0e5854924ff4c691740706318ed85e674846b
AcelyonYT <95740136+AcelyonYT@users.noreply.github.com>
2022-12-17 02:15:11 -0500

    Added a backend part for the website.
    I added in Spring boot for the backend. This will allow me to use Java. I haven't got react and spring boot to communicate yet, but I made a simple test program to test if websites were up and running by making a simple connection and sending a GET request. Depending on the request code it will determine if the site is up or if it is down.

--- src/frontend/javascripts/layout.js
+++ src/frontend/javascripts/layout.js
@@ -2,12 +2,14 @@ import React from "react";
 import {Outlet} from "react-router-dom";
 import NavBar from "./components/navBar/nav";
 import Header from "./layouts/header";
+import Footer from "./layouts/footer";
 
 const Layout = () => {
     return (
         <>
             <NavBar />
             <Header />
+            <Footer />
             <Outlet />
         </>
     );
