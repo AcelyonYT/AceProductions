e94b354c3ed2956b39b633b3290fc6c4c989d51c
AcelyonYT <95740136+AcelyonYT@users.noreply.github.com>
2024-06-11 13:09:16 -0400

    cleaned up code and made game page accessible
    cleaned up the code for the routes, now it will be more scalable, just have to add a new string in the array and it creates it. Also made it so that the game page will now display

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from "./typescript/layout";
 
 function App() {
+    const pathsArray: string[] = [ "/", "/status", "/game" ];
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Layout />}>
-                </Route>
-                <Route path="/status" element={<Layout />}>
-                </Route>
+                {createPaths(pathsArray)}
             </Routes>
         </BrowserRouter>
     );
+}
+
+function createPaths(pathArray: string[]) {
+    let createdPaths: React.JSX.Element[] = pathArray.map<React.JSX.Element>((x, index) => <Route key={index} path={x} element={<Layout />}></Route>)
+    return createdPaths
 }
 
 export default App;
--- src/frontend/typescript/pages/game.tsx
+++ src/frontend/typescript/pages/game.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Game() {
-    
+    return(
+        <div id="gameContent">
+            <p>Hello</p>
+        </div>
+    )
 }
 
 export default Game
